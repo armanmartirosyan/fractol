8ac5b1f9d26ff789c8442f9b755c83b43a9f9c11
Arman <armanmartirosyan81@gmail.com>
2023-03-05 17:23:03 +0400

    tmp

--- include/fractol.h
+++ include/fractol.h
@@ -6,7 +6,7 @@
 /*   By: armartir <armartir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 19:32:07 by armartir          #+#    #+#             */
-/*   Updated: 2023/03/05 14:02:40 by armartir         ###   ########.fr       */
+/*   Updated: 2023/03/05 16:57:03 by armartir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,33 +19,33 @@
 // TODO: Delete stdio library
 #include <stdio.h>
 
-typedef struct	s_data
+# define W_WIDTH 1080
+# define W_HEIGHT 1080
+# define MAX_ITERATIONS 50
+typedef struct s_complex
 {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_lenght;
-	int 	endian;
-}	t_data;
+	double	r;
+	double	i;
+}	t_complex;
 
-typedef struct	s_img
+typedef struct s_params
 {
-	void	*img_ptr;
-	int		*data;
-	int		size_l;
-	int		bpp;
-	int		endian;
-}	t_img;
-
-typedef struct s_mlx
-{
-	void	*mlx;
-	void	*win;
-	t_img	img;
-	int		scale;
-	double	center_y;
-	double	center_x;
-}	t_mlx;
+	void		*mlx;
+	void		*win;
+	void		*img_ptr;
+	char		*addr;
+	int			bpp;
+	int			line_len;
+	int			endian;
+	int			width;
+	int			height;
+	t_complex	min;
+	t_complex	max;
+	int			scale;
+	int			max_iter;
+	int			color;
+	int			cur_iter;
+}	t_params;
 
 
 #endif
--- main.c
+++ main.c
@@ -6,175 +6,105 @@
 /*   By: armartir <armartir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 19:30:23 by armartir          #+#    #+#             */
-/*   Updated: 2023/03/05 15:32:41 by armartir         ###   ########.fr       */
+/*   Updated: 2023/03/05 17:16:14 by armartir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+/*
+for each pixel (Px, Py) on the screen do
+    x0 := scaled x coordinate of pixel (scaled to lie in the Mandelbrot X scale (-2.00, 0.47))
+    y0 := scaled y coordinate of pixel (scaled to lie in the Mandelbrot Y scale (-1.12, 1.12))
+    x := 0.0
+    y := 0.0
+    iteration := 0
+    max_iteration := 1000
+    while (x*x + y*y ≤ 2*2 AND iteration < max_iteration) do
+        xtemp := x*x - y*y + x0
+        y := 2*x*y + y0
+        x := xtemp
+        iteration := iteration + 1
+    
+    color := palette[iteration]
+    plot(Px, Py, color)
+*/
 
-// int rgb_to_int(double r, double g, double b)
-// {
-//     int color = 0;
-//     color |= (int)(b * 255);
-//     color |= (int)(g * 255) << 8;
-//     color |= (int)(r * 255) << 16;
-//     return (color);
-// }
-
-// void	mandelbrod(double x,double y)
-// {
-// 	double	zx;
-// 	double	zy;
-// 	double	tmp;
-// 	int		iter;
-
-// 	iter = 0;
-// 	zx = x;
-// 	zy = y;
-// 	while (pow(x,2) + pow(y, 2) < 4 && iter < 50)
-// 	{
-// 		tmp = x;
-// 		x = pow(x, 2) + pow(y, 2) + zx;
-// 		y = 2 * y * tmp + zy;
-// 	}
+int	mandelbrot(t_complex c, int max_iter)
+{
+	int			i;
+	t_complex	z;
+	double		zr;
+	double		zi;
 	
-	
-// }
-
-// void	my_mlx_pixel_put (t_data *data, int x, int y, int color)
-// {
-// 	char	*dst;
-
-// 	dst = data->addr + (y * data->line_lenght + x * (data->bits_per_pixel / 8));
-// 	*(unsigned int*)dst = color;
-// }
-
-// int main()
-// {
-// 	void	*mlx_ptr;
-// 	void	*win_ptr;
-// 	t_data	img;
-// 	double i = -500;
-// 	double j = 0;
-// 	const double height = 1920;
-// 	const double width = 1080;
-
-
-// 	mlx_ptr = mlx_init();
-// 	win_ptr = mlx_new_window(mlx_ptr, 1920, 1080, "Hello World");
-// 	img.img = mlx_new_image(mlx_ptr, 1920, 1080);
-
-// 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_lenght, &img.endian);
-
-// 	while (i < height)
-// 	{
-// 		j = -500;
-// 		while (j < width)
-// 		{
-// 			if (pow(i,2) + pow(j,2) <= pow (120,2))
-// 				mlx_pixel_put(mlx_ptr, win_ptr, j + height/2, i + width/2, 0x00FFFFFF);
-// 			// my_mlx_pixel_put(&img, i, j, 0x00FF0000);
-// 			j++;
-// 		}
-// 		i++;
-// 	}
-
-// 	// mlx_put_image_to_window(mlx_ptr, win_ptr, img.img, 0, 0);
-// 	mlx_loop(mlx_ptr);
-// 	return (0);
-// }
-
-#include "mlx.h"
-#include <math.h>
-#include <stdlib.h>
-
-typedef struct s_complex
-{
-    double r;
-    double i;
-} t_complex;
-
-typedef struct s_params
-{
-    void *mlx;
-    void *win;
-    void *img;
-    char *addr;
-    int bpp;
-    int line_len;
-    int endian;
-    int width;
-    int height;
-    t_complex min;
-    t_complex max;
-    int max_iterations;
-} t_params;
-
-int mandelbrot(t_complex c, int max_iterations)
-{
-    int i;
-    t_complex z;
-    z.r = 0;
-    z.i = 0;
-
-    for (i = 0; i < max_iterations; i++)
-    {
-        double zr = z.r;
-        double zi = z.i;
-        z.r = zr * zr - zi * zi + c.r;
-        z.i = 2 * zr * zi + c.i;
-        if (z.r * z.r + z.i * z.i > 4)
-            break;
-    }
-    return i;
+	z.r = 0;
+	z.i = 0;
+	i = 0;
+	while (i < max_iter)
+	{
+		zr = z.r;
+		zi = z.i;
+		z.r = zr * zr - zi * zi + c.r;
+		z.i = 2 * zr * zi + c.i;
+		if (z.r * z.r + z.i * z.i > 4)
+			break ;
+		i++;
+	}
+	return (i);
 }
 
-void draw_fractal(t_params *params)
+void	draw_fractol(t_params *params)
 {
-    int x, y;
-    t_complex c;
-    int color;
+	int			x;
+	int			y;
+	t_complex	c;
+	char		*addr;
 
-    for (y = 0; y < params->height; y++)
-    {
-        for (x = 0; x < params->width; x++)
-        {
-            c.r = params->min.r + (params->max.r - params->min.r) * x / (params->width - 1.0);
-            c.i = params->min.i + (params->max.i - params->min.i) * y / (params->height - 1.0);
+	y = 0;
+	while (y < params->height)
+	{
+		x = 0;
+		while (x < params->width)
+		{
+			c.r = params->min.r + (params->max.r - params->min.r) * x / (params->width - 1.0);
+			c.i = params->min.i + (params->max.i - params->min.i) * y / (params->height - 1.0);
 
-            int iterations = mandelbrot(c, params->max_iterations);
-
-            if (iterations == params->max_iterations)
-                color = 0x000000;
-            else
-                color = 0x0000FF * (iterations % 256);
-
-            char *pixel_addr = params->addr + (y * params->line_len + x * (params->bpp / 8));
-            *(unsigned int *)pixel_addr = color;
-        }
-    }
-    mlx_put_image_to_window(params->mlx, params->win, params->img, 0, 0);
+			params->cur_iter = mandelbrot(c, params->max_iter);
+			// if (params->cur_iter == params->max_iter)
+			// 	params->color = 0x000000;
+			// else
+				params->color = 0x0000FF * 50 * params->cur_iter;
+		 	addr = params->addr + (y * params->line_len + x * (params->bpp / 8));
+            *(unsigned int *)addr = params->color;
+			x++;
+		}
+		y++;
+	}
+	mlx_put_image_to_window(params->mlx, params->win, params->img_ptr, 0, 0);
 }
 
-int main()
+void	param_init(t_params	*params)
 {
-    t_params params;
-    params.width = 1080;
-    params.height = 1080;
-    params.min.r = -2.0;
-    params.max.r = 1.0;
-    params.min.i = -1.2;
-    params.max.i = 1.2;
-    params.max_iterations = 50;
+	params->width = W_WIDTH;
+	params->height = W_HEIGHT;
+	params->min.r = -2.0;
+	params->min.i = -1.5;
+	params->max.r = 1.0;
+	params->max.i = 1.5;
+	params->max_iter = MAX_ITERATIONS;
+}
 
-    params.mlx = mlx_init();
-    params.win = mlx_new_window(params.mlx, params.width, params.height, "Mandelbrot");
-    params.img = mlx_new_image(params.mlx, params.width, params.height);
-    params.addr = mlx_get_data_addr(params.img, &params.bpp, &params.line_len, &params.endian);
+int main ()
+{
+	t_params	params;
 
-    draw_fractal(&params);
+	param_init(&params);
+	params.mlx = mlx_init();
+	params.win = mlx_new_window(params.mlx, params.height, params.width, "Mandelbrot");
+	params.img_ptr = mlx_new_image(params.mlx, params.width, params.height);
+	params.addr = mlx_get_data_addr(params.img_ptr, &params.bpp, &params.line_len, &params.endian);
 
-    mlx_loop(params.mlx);
-    return 0;
+	draw_fractol(&params);
+	mlx_loop(params.mlx);
+	return (0);
 }
 
